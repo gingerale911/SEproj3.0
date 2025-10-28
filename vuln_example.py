82e684477307d688641883f6eae07e54346ac98c
gingerale911 <sr.00017000@gmail.com>
2025-10-29 02:35:16 +0530

    Add hardcoded password and eval vulnerability for workflow test

--- vuln_example.py
+++ vuln_example.py
@@ -1,2 +1,8 @@
+# --- Hardcoded password ---
+PASSWORD = "supersecret123"
+
+# --- Dangerous eval ---
+user_code = input("Enter code to run: ")
+eval(user_code)
 # (Copy of your latest vuln_example.py)
 # Paste your latest test file here or let me know if you want me to fetch and copy from the previous project.
